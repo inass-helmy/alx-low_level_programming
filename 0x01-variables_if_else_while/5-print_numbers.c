21c806f6bf039e918cfe795d4ddba7b7899876bc
inass-helmy <inass.helmy@gmail.com>
2023-03-20 00:09:24 -0700

    fix code

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: a program that prints all single digit numbers of base 10 starting from 0,
- * followed by a new line.
+ * Description: a program that prints all single digit numbers of
+ * base 10 starting from 0, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
