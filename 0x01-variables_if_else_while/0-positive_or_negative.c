90120718f79f29eef79a8ed1a9ac1f0890f1752a
inass-helmy <inass.helmy@gmail.com>
2023-03-19 20:00:55 -0700

    fix Betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**  main - determine if number is positive or negative
- *  Description: program will assign a random number to the variable n each time 
+/**
+ * main - determine if number is positive or negative
+ * Description: program will assign a random number to the variable n each time 
  * it is executed.
  * Return: Always 0 (Success)
- * */
+ */
 
 int main(void) 
 { 
        	int n;
+
     	srand(time(0)); 
        	n = rand() - RAND_MAX / 2;  
 	if (n == 0)   
