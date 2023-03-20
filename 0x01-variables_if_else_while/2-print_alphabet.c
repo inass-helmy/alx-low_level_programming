85c613b96eb8f089776787c17384537d01ed785d
inass-helmy <inass.helmy@gmail.com>
2023-03-19 20:21:37 -0700

    print last letters in alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print last last letters of the alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
