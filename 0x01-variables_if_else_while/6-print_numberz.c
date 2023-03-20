70603d379ef120b970a1907fbe756b3016803d90
inass-helmy <inass.helmy@gmail.com>
2023-03-20 00:16:55 -0700

    remove whitespace

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: a program that prints all single digit numbers of 
+ * Description: a program that prints all single digit numbers of
  * base 10 starting from 0, followed by a new line
  *
  * Return: A;ways 0 (Success)
