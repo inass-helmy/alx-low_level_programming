b55f814f3f96551b76ebbba6163b140b7b9b2620
inass-helmy <inass.helmy@gmail.com>
2023-03-19 19:04:40 -0700

    print sizes of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the size of various types on the computer which compiles and run on
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of char : %d byte(s)\n", (int) sizeof(char));
+	printf("Size of int : %d byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int : %d byte(s)\n", (int) sizeof(long int));
+	printf("Size of a long long int : %d byte(s)\n", (int) sizeof(long long int));
+	printf("Size of a float : %d byte(s)\n", (int) sizeof(float));
+
+	return (0);
+}
