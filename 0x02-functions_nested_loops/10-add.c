7c31523366be369ce68a8c91dc5438a5cb8781a7
inass-helmy <inass.helmy@gmail.com>
2023-03-22 12:42:33 -0700

    fix style

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @n an int
+ * @m an int
+ *
+ * Return: n+m
+ *
+*/
+int add(int n, int m)
+{
+	int sum;
+	
+	sum = n + m;
+
+	return (sum);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,19 +10,19 @@ void times_table(void)
 {
 	int unites, tens, row, col, product;
 
-	for( row =0; row <=9; row++ )
+	for(row = 0; row <= 9; row++)
 	{
-		for( col =0; col <=9; col++ )
+		for(col = 0; col <= 9; col++)
 		{
 			product = row * col;
 			tens = product / 10;
 			unites = product % 10;
 
-			if( col == 0 )
+			if(col == 0)
 			{
 				_putchar('0');
 			}
-			else if ( product < 10 )
+			else if (product < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
