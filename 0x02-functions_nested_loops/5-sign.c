bec4e6b3a8e586898cdfdae22c437df3fd6f515d
inass-helmy <inass.helmy@gmail.com>
2023-03-21 21:13:02 -0700

    fix file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,22 +9,22 @@
  * -1 and prints - if n is less than zero
  *
 */
-int print_sign (int n)
+int print_sign(int n)
 {
-	if ( n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if ( n == 0)
-	{
-		_putchar(0);
-		return (0);
-	}
-	else
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
 	}
 
 }
