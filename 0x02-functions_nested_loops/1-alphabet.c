8c95f4284323b66f4b559dcc1d2d3d3979fa3000
inass-helmy <inass.helmy@gmail.com>
2023-03-21 20:08:35 -0700

    creat function to print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * function print_alphabet
+ *
+ * Description: print alphabet in lowercase
+ *
+ * Return: void
+*/
+
+void print_alphabet (void) {
+   char letter;
+
+   for(letter = 'a' ; letter <= 'z' ; letter++) {
+     _putchar(letter);
+   }
+   _putchar('\n');
+
+}
