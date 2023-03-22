592db974c979bc596efea333972f7209b92c721d
inass-helmy <inass.helmy@gmail.com>
2023-03-22 14:12:42 -0700

    fix code

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,16 +11,18 @@ int main(void)
 {
 	long int first, second, sum;
 	int count;
+	char ch
 
 	sum = 0;
 	first = 0;
 	second = 1;
 	count = 0;
+	ch = 0x2C
 
 	while (count < 98)
 	{
 		sum = first + second;
-		_putchar(sum + ", ");
+		_putchar(sum + ch);
 		first = second;
 		second = sum;
 
