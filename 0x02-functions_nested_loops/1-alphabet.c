bff9f66c09f1f11457af932e204a822b3bc61c94
inass-helmy <inass.helmy@gmail.com>
2023-03-21 20:13:24 -0700

    creat function to print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,13 @@
  * Return: void
 */
 
-void print_alphabet (void) {
-   char letter;
-
-   for(letter = 'a' ; letter <= 'z' ; letter++) {
-     _putchar(letter);
-   }
-   _putchar('\n');
+void print_alphabet(void)
+{
+	char letter;
+	for(letter = 'a' ; letter <= 'z' ; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
 
 }
